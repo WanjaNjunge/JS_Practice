00f5aa5e98576017d48588154ffc86a67edb34b6
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-10-30 15:40:12 +0300

    Done with arrays methods lab

---
+++ DSA/arrays/lab1.js
@@ -0,0 +1,43 @@
+cats = ["Milo", "Otis", "Garfield"];
+
+/*function destructivelyAppendCat(name) {
+    return cats.push(name)
+};
+console.log(destructivelyAppendCat('Ralph'));*/
+
+/*function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+};
+console.log(destructivelyPrependCat("Bob"));*/
+
+/*function destructivelyRemoveLastCat() {
+    return cats.pop()
+};
+console.log(destructivelyRemoveLastCat());*/
+
+/*function destructivelyRemoveFirstCat() {
+    return cats.shift()
+};
+console.log(destructivelyRemoveFirstCat());*/
+
+function appendCat(name) {
+    return newCat = [...cats, name]
+};
+console.log(appendCat("Broom"));
+console.log(cats)
+
+function prependCat(name) {
+    return newCat = [name, ...cats]
+};
+console.log(prependCat("Arnold"));
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+};
+console.log(removeLastCat());
+
+function removeFirstCat() {
+    return cats.slice(1)
+};
+console.log(removeFirstCat());
+

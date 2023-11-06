45190cf253d1f0754b977fb32dd21b06f419ad3a
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-11-06 14:19:05 +0300

    Done with working with Arrays and objects

--- DSA/loops/loops.js
+++ DSA/loops/loops.js
@@ -26,4 +26,104 @@ function loopThroughObj(obj) {
     }
 }
 
-loopThroughObj(oneBook);
+// loopThroughObj(oneBook);
+
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }*/
+
+  /*function writeCards(array, eventName) {
+    let messageArray = [];
+    for(let i = 0; i < array.length; i++) {
+        
+        messageArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift`);
+        
+    }
+
+    return messageArray;
+
+    
+  }
+
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+  function countDown(number) {
+    let i = number
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    }
+  }  
+
+  countDown(10) */
+
+  //RECURSION
+  
+  // let counter = 0;
+
+  function deepIterator(target) {
+    // console.log('Argument: ', target);
+    // console.log(counter++);
+
+    if (Array.isArray(target)) {
+        for (const element of target) {
+            deepIterator(element);
+        }
+    } 
+    
+    else if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+        } else {
+        console.log(target);
+        }
+    };
+
+  const numbers = [1, [2, [4, [5, [6]], 3]]];
+
+  const userInfo = {
+    firstName: "Avi",
+    lastName: "Flombaum",
+    company: {
+      name: "Flatbook Labs",
+      jobTitle: "Developer Apprentice",
+    },
+    friends: [
+      {
+        firstName: "Nancy",
+        lastName: "Burgess",
+        company: {
+          name: "Flatbook Labs",
+          jobTitle: "Developer Apprentice",
+        },
+      },
+      {
+        firstName: "Corinna",
+        lastName: "Jackson",
+        company: {
+          name: "Flatbook Labs",
+          jobTitle: "Lead Developer",
+        },
+      },
+    ],
+    projects: [
+      {
+        title: "Flatbook",
+        description:
+          "The premier Flatiron School-based social network in the world.",
+      },
+      {
+        title: "Scuber",
+        description:
+          "A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.",
+      },
+    ],
+  };
+  
+
+  
+
+  // deepIterator(numbers);
+  deepIterator(userInfo);
+

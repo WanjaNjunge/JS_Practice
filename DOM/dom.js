9d73b2864ebec8c467ef9a7fd966275506ae68b1
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-12-22 12:40:21 +0300

    Done with DOM manipulation

---
+++ DOM/dom.js
@@ -0,0 +1,13 @@
+const main = document.querySelector('main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = "victory";
+
+newHeader.textContent = 'Wanja is the champion';
+
+document.body.appendChild(newHeader)
+
